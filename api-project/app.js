54ed6fdd55cb0fc5efc81067e5a8c3829fff7fca
tckastanek <tckastanek@gmail.com>
2017-05-18 14:44:55 -0400

    working on appstate and ajax

--- api-project/app.js
+++ api-project/app.js
@@ -1,7 +1,32 @@
 //APP INITIALISES
 const appState={
-
+  playlist = [],
+  currentQuery = null,
+  nextQuery = null,
+  prevQuery = null,
 }
+// AJAX
+function fetchPlaylists(searchTerm, callback) {
+  const BASE_API = https://api.spotify.com/v1/search;
+
+  const params = {
+    q: searchTerm,
+    type: playlist,
+    limit: 5,
+  };
+
+  $.ajax({
+    method: 'GET',
+    url: BASE_API,
+    data: params,
+    success: response => {
+      const playlists = response.items.map(item => {
+        
+      })
+    }
+  })
+}
+
 //STATE MODS
 function nameHere(state){
     //state.addHere=
