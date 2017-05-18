f9d135d27f600db8fbde3b24850ae884bad84509
Avizc <avirzajac@gmail.com>
2017-05-18 12:36:50 -0400

    Adding more base code for files

---
+++ api-project/app.js
@@ -0,0 +1,24 @@
+//APP INITIALISES
+const appState={
+
+}
+//STATE MODS
+function nameHere(state){
+    //state.addHere=
+}
+//RENDER
+function render(state){
+
+}
+//EVENT HANDLERS
+function eventHandlers(){
+    $('.HTMLCLASSSTUFF').click(function(event){
+        // CHANGE STATE WITH STATE MOD FUNCTION
+        // INVOKE RENDER FUNCTION
+    });
+}
+//RUN THIS ENTIRE CUTE THING
+$(function(){
+    eventHandlers();
+    render(appState, '');
+});
