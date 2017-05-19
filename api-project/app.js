49e0d4fddee593a40db3ba2aefbd25d37d26a896
tckastanek <tckastanek@gmail.com>
2017-05-19 12:33:44 -0400

    working sort of

--- api-project/app.js
+++ api-project/app.js
@@ -1,41 +1,41 @@
 // APP INITIALISES
-const testData = [
-  {
-    height: null,
-    name: "It's Lit 🔥",
-    spotify: "http://open.spotify.com/user/marquez.anthony96/playlist/4x5uuUCh7VxyiuCV1gGejF",
-    url: "https://u.scdn.co/images/pl/default/733dd40bc3bbe082264eff3b8d2ba6350e9a872d",
-    width: null,
-  },
-  {
-    height: 300,
-    name: "It's lit",
-    spotify: "http://open.spotify.com/user/spotify/playlist/37i9dQZF1DWWmG4xgoLORa",
-    url: "https://i.scdn.co/image/b6e4f3def28f6ed06bbb10d41e9a389dd04f6ee7",
-    width: 300,
-  },
-  {
-    height: 300,
-    name: "It's lit",
-    spotify: "http://open.spotify.com/user/spotify/playlist/37i9dQZF1DWWmG4xgoLORa",
-    url: "https://i.scdn.co/image/b6e4f3def28f6ed06bbb10d41e9a389dd04f6ee7",
-    width: 300,
-  },
-  {
-    height: 300,
-    name: "It's lit",
-    spotify: "http://open.spotify.com/user/spotify/playlist/37i9dQZF1DWWmG4xgoLORa",
-    url: "https://i.scdn.co/image/b6e4f3def28f6ed06bbb10d41e9a389dd04f6ee7",
-    width: 300,
-  },
-  {
-    height: 300,
-    name: "It's lit",
-    spotify: "http://open.spotify.com/user/spotify/playlist/37i9dQZF1DWWmG4xgoLORa",
-    url: "https://i.scdn.co/image/b6e4f3def28f6ed06bbb10d41e9a389dd04f6ee7",
-    width: 300,
-  },
-]
+// const testData = [
+//   {
+//     height: null,
+//     name: "It's Lit 🔥",
+//     spotify: "http://open.spotify.com/user/marquez.anthony96/playlist/4x5uuUCh7VxyiuCV1gGejF",
+//     url: "https://u.scdn.co/images/pl/default/733dd40bc3bbe082264eff3b8d2ba6350e9a872d",
+//     width: null,
+//   },
+//   {
+//     height: 300,
+//     name: "It's lit",
+//     spotify: "http://open.spotify.com/user/spotify/playlist/37i9dQZF1DWWmG4xgoLORa",
+//     url: "https://i.scdn.co/image/b6e4f3def28f6ed06bbb10d41e9a389dd04f6ee7",
+//     width: 300,
+//   },
+//   {
+//     height: 300,
+//     name: "It's lit",
+//     spotify: "http://open.spotify.com/user/spotify/playlist/37i9dQZF1DWWmG4xgoLORa",
+//     url: "https://i.scdn.co/image/b6e4f3def28f6ed06bbb10d41e9a389dd04f6ee7",
+//     width: 300,
+//   },
+//   {
+//     height: 300,
+//     name: "It's lit",
+//     spotify: "http://open.spotify.com/user/spotify/playlist/37i9dQZF1DWWmG4xgoLORa",
+//     url: "https://i.scdn.co/image/b6e4f3def28f6ed06bbb10d41e9a389dd04f6ee7",
+//     width: 300,
+//   },
+//   {
+//     height: 300,
+//     name: "It's lit",
+//     spotify: "http://open.spotify.com/user/spotify/playlist/37i9dQZF1DWWmG4xgoLORa",
+//     url: "https://i.scdn.co/image/b6e4f3def28f6ed06bbb10d41e9a389dd04f6ee7",
+//     width: 300,
+//   },
+// ]
 
 const appState = {
   emojis:
@@ -46,12 +46,12 @@ const appState = {
       relax: 'chill',
       sad: 'sad'
     },
-  playlist: testData,
-  //selectEmoji: false,
+  playlist: [],
   currentQuery: null,
   nextQuery: null,
   prevQuery: null,
 }
+
 // AJAX
 function fetchPlaylists(searchTerm, callback, pageURL) {
   const BASE_API = 'https://api.spotify.com/v1/search';
@@ -63,7 +63,7 @@ function fetchPlaylists(searchTerm, callback, pageURL) {
     limit: 5,
   };
 
-  // this works
+  // this works by itself
   // $.ajax({
   //   method: 'GET',
   //   url: BASE_API,
@@ -79,7 +79,7 @@ function fetchPlaylists(searchTerm, callback, pageURL) {
     url: BASE_API,
     data: params,
     success: response => {
-      loadData(editData(response));
+      callback(editData(response));
     },
   });
   //load playlists on prev or next button click
@@ -88,10 +88,10 @@ function fetchPlaylists(searchTerm, callback, pageURL) {
     url: PREV_NEXT,
     //processData: false, //deactivate need to process params for query string
     success: response => {
-      loadData(editData(response));
+      callback(editData(response));
     },
   });
-  //initLoad;
+  //Conditional for which AJAX call to make and render.;
   (searchTerm) ? initLoad : btnLoad;
 };
 
@@ -99,6 +99,7 @@ function fetchPlaylists(searchTerm, callback, pageURL) {
 //CALLBACK FUNCTIONS
 function editData(response) {
 let obj = response['playlists'];
+console.log(obj);
 const items = obj['items'].map(item => {
   const {spotify} = item['external_urls'];
   const {height, url, width} = item['images'][0];
@@ -143,7 +144,7 @@ function render(state){
     resultPlaylists += '<p>No results</p>';
   };
 
-  (true) ? $('.js-next').show() : $('.js-next').hide();
+  (state.nextQuery) ? $('.js-next').show() : $('.js-next').hide();
   (state.prevQuery) ? $('.js-prev').show() : $('.js-prev').hide();
   $('.show-playlists').html(resultPlaylists);
 };
@@ -155,10 +156,10 @@ function eventHandlers(){
       clickEmoji(appState, emojiType);
       fetchPlaylists(appState.currentQuery, loadData);
     });
-    $('.js-prev').click(function(event) {
+    $('.js-nav-buttons').on('click', '.js-prev', function(event) {
       fetchPlaylists(null, loadData, appState.prevQuery);
     })
-    $('.js-next').click(function(event) {
+    $('.js-nav-buttons').on('click', '.js-next', function(event) {
       fetchPlaylists(null, loadData, appState.nextQuery);
     })
 };
